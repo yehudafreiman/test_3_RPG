7fe2acea29d4e99113df3aa531d4e48fb99345cb
Yehuda Freiman <yehfre@icloud.com>
2025-11-06 11:59:04 +0200

    add func

--- game.py
+++ game.py
@@ -4,18 +4,54 @@ from core.orc import Orc
 from core.goblin import Goblin
 
 class Game:
-
     def start(self):
-        pass
+        while True:
+            select = Game.show_menu(self)
+            if select:
+                print("Start! ->")
+
+                # turn
+                player = Game.create_player(self)
+                print(f"player -> {player.name}")
+                monster = Game.choose_random_monster(self)
+                print(f"monster -> {monster.name}")
+
+                player_roll = Game.roll_dice(self,6)
+                player_roll += player.speed
+                print(f"player result roll -> {player_roll}")
+                monster_roll = Game.roll_dice(self,6)
+                monster_roll += monster.speed
+                print(f"monster result roll -> {monster_roll}")
+
+                if player_roll > monster_roll:
+                    current_attacker = player
+                elif player_roll < monster_roll:
+                    current_attacker = monster
+                else:
+                    current_attacker = player
+
+                # attack
+
+            else:
+                print("End!")
+                exit()
+
+
+
+
 
     def show_menu(self):
-        user_choice = input("Enter 'fight' or 'quit'")
-        return user_choice
+        user_choice = input("Enter fight or quit: ")
+        if user_choice == "fight":
+            return True
+        if user_choice == "quit":
+            return False
+        else:
+            print(f"Invalid input try again {Game.show_menu(self)}")
 
     def create_player(self):
         player = Player("Harry Potter")
         return player
-
 
     def choose_random_monster(self):
         monster = choice(["Orc", "Goblin"])
@@ -25,14 +61,17 @@ class Game:
             monster = Goblin("Alakazam")
         return monster
 
-    def battle(self,player, monster):
-        pass
-
     def roll_dice(self,sides):
         if sides == 6:
             result = randint(0, 6)
         else:
             result = randint(0, 20)
+        return result
+
+
+    def battle(self,player, monster):
+        pass
+
 
 
 
--- main.py
+++ main.py
@@ -3,3 +3,8 @@ from core.player import Player
 from core.orc import Orc
 from core.goblin import Goblin
 from game import Game
+
+if __name__ == "__main__":
+    my_game = Game()
+    my_game.start()
+
